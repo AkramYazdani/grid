76861f552b896f7974ccdd42819e0ac80dbc481d
Paul Murrell <paul@stat.auckland.ac.nz>
2002-12-20 00:00:00 +0000

    version 0.7-4

--- R/primitives.R
+++ R/primitives.R
@@ -194,7 +194,9 @@ grid.text <- function(label, x=unit(0.5, "npc"), y=unit(0.5, "npc"),
     x <- unit(x, default.units)
   if (!is.unit(y))
     y <- unit(y, default.units)
-  txt <- list(label=as.character(label), x=x, y=y, gp=gp,
+  if (!is.expression(label))
+    label <- as.character(label)
+  txt <- list(label=label, x=x, y=y, gp=gp,
               just=just, rot=rot, check.overlap=check.overlap,
               vp=vp)
   cl <- "text"
